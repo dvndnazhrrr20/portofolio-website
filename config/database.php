d2a9e312db97afc79132dbc1a31d6358dd588099
dvndnazhrrr20 <dvndnazhr20@gmail.com>
2025-09-30 12:20:13 +0700

    update deploy database to dbeaver

--- config/database.php
+++ config/database.php
@@ -1,13 +1,14 @@
 <?php
-// Pengaturan Database
-$host = 'localhost';
-$db_name = 'portfolio_db';
-$username = 'root'; // Default username XAMPP
-$password = '';   // Default password XAMPP kosong
+// Pengaturan Database dari Variabel Lingkungan Railway
+$host = getenv('MYSQLHOST');
+$db_name = getenv('MYSQLDATABASE');
+$username = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+$port = getenv('MYSQLPORT');
 
 // Buat koneksi
 try {
-    $pdo = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    $pdo = new PDO("mysql:host={$host};port={$port};dbname={$db_name}", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Koneksi ke database gagal: " . $e->getMessage());
